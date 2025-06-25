4139ad765dd867b4c1e46204d18172197fc3204c
Udita Panda <uditaaa29@gmail.com>
2025-06-26 01:58:39 +0530

    user model saved commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -80,13 +80,10 @@ userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
         {
             _id: this._id,
-            email: this.email,
-            username: this.username,
-            fullname: this.fullname
         },
-        process.env.ACCESS_TOKEN_SECRET,
+        process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
     )
 }
