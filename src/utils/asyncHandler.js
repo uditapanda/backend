f6012beb073b12e9a3045e82a16b9033c64b9710
Udita Panda <uditaaa29@gmail.com>
2025-06-28 02:56:30 +0530

    configuration complete

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,6 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next))
-        .catch((error) => next(error))
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
     }
 }
 
@@ -9,7 +8,7 @@ export {asyncHandler}
 
 /*
  * This function is a middleware for handling asynchronous operations in Express.js.
- * It takes a function `fn` as an argument and returns a new function that handles errors.
+ * It takes a function fn as an argument and returns a new function that handles errors.
  * If an error occurs, it sends a JSON response with the error message and status code.
  
 const asyncHandler = (fn) => async (req, res, next) => {
